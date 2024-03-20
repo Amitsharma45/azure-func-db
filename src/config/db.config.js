2b879a557e3f8bffb58da4a93c83cdd2a1984969
Akash Jha <akashjha2671@gmail.com>
2024-03-20 18:58:02 +0530

    DB Connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,51 +1,67 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 // const logger = require('../logger/api.logger');
 
-const connect = () => {
+const connect = async () => {
+  // const hostName = "127.0.0.1";
+  // const userName = "root";
+  // const password = "Amit@2001";
+  // const database = "symphome_development";
+  // const dialect = "mysql";
 
-    // const hostName = "127.0.0.1"; 
-    // const userName = "root";
-    // const password = "Amit@2001";
-    // const database = "symphome_development";
-    // const dialect = "mysql";
+  const hostName = "onelabdbserver.database.windows.net";
+  const userName = "adminOnelab";
+  const password = "Onelab0504#";
+  const database = "onlabtestingdb";
+  const dialect = "mssql";
 
-    const hostName = "onelabdbserver.database.windows.net"; 
-    const userName = "adminOnelab";
-    const password = "Onelab0504#";
-    const database = "onlabtestingdb";
-    const dialect = "mysql";
+  console.log("dialect ", dialect);
 
-    console.log('dialect ', dialect)
+  //   const sequelize = new Sequelize(database, userName, password, {
+  //     host: hostName,
+  //     dialect: dialect,
+  //     operatorsAliases: false,
+  //     options: {
+  //       encrypt: true, // For Azure SQL Database
+  //       trustServerCertificate: true, // For Azure SQL Database
+  //     },
+  //     dialectOptions: {
+  //       encrypt: true,
+  //     },
+  //     pool: {
+  //       max: 10,
+  //       min: 0,
+  //       acquire: 20000,
+  //       idle: 5000,
+  //     },
+  //     port: 1433,
+  //   });
 
-    const sequelize = new Sequelize(database, userName, password, {
-        host: hostName,
-        dialect: dialect,
-        operatorsAliases: false,
-        options: {
-            encrypt: true, // For Azure SQL Database
-            trustServerCertificate: true // For Azure SQL Database
-        },
-        dialectOptions: {
-            encrypt: true
-        },
-        pool: {
-            max: 10,
-            min: 0,
-            acquire: 20000,
-            idle: 5000
-        },
-        port:1433
-    });
+  const sequelize = new Sequelize(
+    "onlabtestingdb",
+    "adminOnelab",
+    "Onelab0504#",
+    {
+      host: "onelabdbserver.database.windows.net",
+      port: 1433,
+      dialect: "mssql",
+    }
+  );
 
-    const db = {};
-    db.Sequelize = Sequelize;
-    db.sequelize = sequelize;
-    db.tasks = require("../model/task.model")(sequelize, DataTypes, Model);
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 
-    return db;
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.tasks = require("../model/task.model")(sequelize, DataTypes, Model);
 
-}
+  return db;
+};
 
 module.exports = {
-    connect
-}
+  connect,
+};
--- src/repository/task.repository.js
+++ src/repository/task.repository.js
@@ -1,73 +1,79 @@
-const { connect } = require('../config/db.config');
+const { connect } = require("../config/db.config");
 // const logger = require('../logger/api.logger');
 
-
 class TaskRepository {
+  db = {};
 
-    db = {};
+  constructor() {
+    (async () => {
+      try {
+        this.db = await connect();
+        console.log("db connected success");
+      } catch (error) {
+        console.error("Error connecting to the database:", error);
+        // Handle the error accordingly
+      }
+    })();
+  }
 
-    constructor() {
-        this.db = connect();
-        console.log("db connected success")
+  async getTasks() {
+    try {
+      const tasks = await this.db.tasks.findAll();
+      console.log("tasks:::", tasks);
+      return tasks;
+    } catch (err) {
+      console.log(err);
+      return err;
     }
+  }
 
-    async getTasks() {
-        
-        try {
-            const tasks = await this.db.tasks.findAll();
-            console.log('tasks:::', tasks);
-            return tasks;
-        } catch (err) {
-            console.log(err);
-            return err;
+  async createTask(task, context) {
+    let data = {};
+    try {
+      context.log("-----task.repository.js----", task);
+      data = await this.db.tasks.create(task);
+    } catch (err) {
+      // logger.error('Error::' + err);
+      // context.log(err);
+      console.log("-----error----38", err);
+      return err;
+    }
+    return data;
+  }
+
+  async updateTask(task) {
+    let data = {};
+    try {
+      console.log("task:::", task);
+      // task.updateddate = new Date().toISOString();
+      data = await this.db.tasks.update(
+        { ...task },
+        {
+          where: {
+            id: task.id,
+          },
         }
+      );
+    } catch (err) {
+      // logger.error('Error::' + err);
     }
+    return data;
+  }
 
-    async createTask(task,context) {
-        let data = {};
-        try {
-            context.log("-----task.repository.js----",task)
-            data = await this.db.tasks.create(task);
-        } catch(err) {
-            // logger.error('Error::' + err);
-            // context.log(err);
-            console.log("-----error----38");
-            return err;
-        }
-        return data;
+  async deleteTask(taskId) {
+    let data = {};
+    try {
+      data = await this.db.tasks.destroy({
+        where: {
+          id: taskId,
+        },
+      });
+    } catch (err) {
+      // logger.error('Error::' + err);
     }
-
-    async updateTask(task) {
-        let data = {};
-        try {
-            console.log('task:::', task)
-            // task.updateddate = new Date().toISOString();
-            data = await this.db.tasks.update({...task}, {
-                where: {
-                    id: task.id
-                }
-            });
-        } catch(err) {
-            // logger.error('Error::' + err);
-        }
-        return data;
-    }
-
-    async deleteTask(taskId) {
-        let data = {};
-        try {
-            data = await this.db.tasks.destroy({
-                where: {
-                    id: taskId
-                }
-            });
-        } catch(err) {
-            // logger.error('Error::' + err);
-        }
-        return data;
-        return {status: `${data.deletedCount > 0 ? true : false}`};
-    }
-
+    return data;
+    return { status: `${data.deletedCount > 0 ? true : false}` };
+  }
 }
 
 module.exports = new TaskRepository();
